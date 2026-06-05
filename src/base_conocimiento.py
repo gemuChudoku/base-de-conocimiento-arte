9e564ab83401a48d651b30440992215869fb9855
gemuChudoku <carloslosadadiaz2005@gmail.com>
2026-06-04 20:45:04 -0500

    Update base_conocimiento.py

--- src/base_conocimiento.py
+++ src/base_conocimiento.py
@@ -488,6 +488,14 @@ REGLAS = [
     "certeza": 0.95,
     "justificacion": "Maya es uno de los estándares de la industria cinematográfica para animación y efectos visuales.",
     },
+     {
+        "nombre": "R48",
+        "descripcion": "Redes sociales + ilustración + poco tiempo → Ibis Paint",
+        "condiciones": [ ("hace_ilustracion", True), ("tiempo_poco", True), ("hace_vectorial", True), ("tiene_tablet_android", True), ("tiene_ipad", True), ("tiene_iphone", True), ("tiempo_moderado", True), ("hace_concept_art", True), ("hace_manga_comic",True)],
+        "conclusion": ("recomendar_ibis_paint", True),
+        "certeza": 0.83,
+        "justificacion": "Ibis Paint permite crear ilustraciones vectoriales de forma rápida desde cualquier dispositivo.",
+    }
 
 
 ]
